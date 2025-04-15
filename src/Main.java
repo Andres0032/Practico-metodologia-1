bfb058a6d053dbf7f48b6364a9438651e6a58ee1
Andres0032 <pirjf359@gmail.com>
2025-04-15 10:17:03 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+/*
+    Entrada:
+            - Numero entero n.
+            Salida:
+            - Retornara booleano que indica si es primo o no.
+    Pre condicion:
+            - N > 0
+    Post codiciones:
+            - Retornara un valor booleano verdadero si el numero es un primo sino retornara falso.
+*/
+
+
+
+
+}
